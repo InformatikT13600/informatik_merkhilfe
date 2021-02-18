829961cc07e2a025c7bbe97a31e5a9e2deed94e1
Sether701 <sether017@gmail.com>
2021-02-18 09:45:29 +0100

    created routing transition object

---
+++ lib/shared/routingTransition.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';class RoutingTransition extends PageRouteBuilder {
+  final Widget page;
+  RoutingTransition({this.page})
+      : super(
+    pageBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+        ) =>
+    page,
+    transitionsBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+        Widget child,
+        ) =>
+        FadeTransition(
+          opacity: animation,
+          child: child,
+        ),
+  );
+}

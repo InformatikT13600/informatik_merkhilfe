fafb9a7b3d5ed640455dca660658b0826298e4dd
Sether701 <sether017@gmail.com>
2021-02-18 09:45:41 +0100

    created home screen and implemented basic routing

---
+++ lib/views/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:informatik_merkhilfe/main.dart';
+import 'package:informatik_merkhilfe/shared/styles.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: colorMainBackground,
+      appBar: AppBar(
+        title: Text('Informatik Merkhilfe'),
+        centerTitle: true,
+        backgroundColor: colorMainAppbar,
+        automaticallyImplyLeading: false,
+      ),
+    );
+  }
+}
+

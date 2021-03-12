59a257405c76cc78ed998307a193a066ec90e162
Sether701 <sether017@gmail.com>
2021-03-12 09:45:05 +0100

    rebuild buildButton functions in styles

--- lib/shared/styles.dart
+++ lib/shared/styles.dart
@@ -4,43 +4,41 @@ final Color colorMainBackground = Color(0xff1E1E5A);
 final Color colorMainAppbar = Color(0xff333399);
 final Color colorRed = Color(0xffD25A5A);
 
+enum ButtonShape {
+  RECTANGULAR,
+  ELLIPTICAL,
+}
 
-Widget buildRectangularButton(String buttonText) {
+Widget _buildButton({String buttonText = "", ButtonShape shape = ButtonShape.RECTANGULAR, Color color = Colors.white, @required Function onPressed}) {
+
+  double borderRadius = 0;
+
+  switch(shape) {
+    case ButtonShape.RECTANGULAR: borderRadius = 25; break;
+    case ButtonShape.ELLIPTICAL: borderRadius = 50; break;
+    default: break;
+  }
 
   return Container(
     constraints: BoxConstraints(minHeight: 100, minWidth: 300, maxHeight: 100, maxWidth: 300),
     decoration: BoxDecoration(
-      border: Border.all(color: colorRed, width: 6),
-      borderRadius: BorderRadius.circular(25),
+      border: Border.all(color: color, width: 6),
+      borderRadius: BorderRadius.circular(borderRadius),
     ),
     child: TextButton(
-      onPressed: () {print('test');},
+      onPressed: () => {onPressed()},
       child: Text(
         buttonText,
         style: TextStyle(color: Colors.white, fontSize: 34, fontWeight: FontWeight.normal),
       ),
     ),
   );
-
 }
 
-Widget buildEllipticalButton(String buttonText) {
+Widget buildButtonRectangular({String buttonText = "", Color color = Colors.white, @required Function onPressed}) {
+  return _buildButton(buttonText: buttonText, color: color, onPressed: onPressed, shape: ButtonShape.RECTANGULAR);
+}
 
-  return Container(
-    constraints: BoxConstraints(minHeight: 100, maxHeight: 100, minWidth: 300, maxWidth: 300),
-    decoration: BoxDecoration(
-      border: Border.all(color: colorRed, width: 6),
-      borderRadius: BorderRadius.circular(50),
-    ),
-    child: TextButton(
-      onPressed: () {print('test');},
-      child: Text(
-          buttonText,
-        style: TextStyle(color: Colors.white, fontSize: 34, fontWeight: FontWeight.normal),
-      ),
-    ),
-  );
-
-  return Container();
-
+Widget buildButtonElliptical({String buttonText = "", Color color = Colors.white, @required Function onPressed}) {
+  return _buildButton(buttonText: buttonText, color: color, onPressed: onPressed, shape: ButtonShape.ELLIPTICAL);
 }

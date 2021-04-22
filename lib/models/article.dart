5e5c3e39fad11c924603fb165aa34c544f9975a5
Sether701 <sether017@gmail.com>
2021-04-22 08:50:51 +0200

    deserialize tag list in article constructor

--- lib/models/article.dart
+++ lib/models/article.dart
@@ -5,6 +5,7 @@ class Article {
   int orderPriority;
   String name;
   List<String> content = [];
+  List<String> tags = [];
 
   Article(this.language, this.category, this.name, this.content, this.orderPriority);
 
@@ -15,7 +16,10 @@ class Article {
     category = json['category'];
 
     List<dynamic> contentList = json['content'];
-    for(String contentLine in contentList) content.add(contentLine);
+    contentList.forEach((contentLine) => content.add(contentLine));
+
+    List<dynamic> tagList = json['tags'];
+    tagList.forEach((tag) => content.add(tag));
   }
 
   bool isValid() => name.isNotEmpty && content.isNotEmpty && category.isNotEmpty && language.isNotEmpty;
